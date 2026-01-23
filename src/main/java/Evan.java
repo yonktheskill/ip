ee6080fcd8b942e633417b0cf56bf85082e003e1
phluidhongxu <50507894+yonktheskill@users.noreply.github.com>
2026-01-23 12:49:11 +0800

    2

--- src/main/java/Evan.java
+++ src/main/java/Evan.java
@@ -9,6 +9,9 @@ public class Evan {
         System.out.println(lineSeparator);
 
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("bye")) {
@@ -16,10 +19,21 @@ public class Evan {
                 System.out.println("Don't leave me ):");
                 System.out.println(lineSeparator);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(lineSeparator);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + tasks[i]);
+                }
+                System.out.println(lineSeparator);
+            } else {
+                if (taskCount < 100) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                }
+                System.out.println(lineSeparator);
+                System.out.println(" added: " + input);
+                System.out.println(lineSeparator);
             }
-            System.out.println(lineSeparator);
-            System.out.println(" " + input);
-            System.out.println(lineSeparator);
         }
         scanner.close();
     }
