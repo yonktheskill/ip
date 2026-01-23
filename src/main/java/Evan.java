d7516d9c253ade3e281294d9b4b382e7931f44ee
phluidhongxu <50507894+yonktheskill@users.noreply.github.com>
2026-01-23 12:40:06 +0800

    part 0

---
+++ src/main/java/Evan.java
@@ -0,0 +1,11 @@
+public class Evan {
+    public static void main(String[] args) {
+        String greeting = "____________________________________________________________\n"
+                + " Hello! I'm Evan\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(greeting);
+    }
+}
