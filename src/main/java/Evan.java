28c1dd1407470f4b724e770bb8d1595b9fc5c2d0
phluidhongxu <50507894+yonktheskill@users.noreply.github.com>
2026-01-23 12:45:33 +0800

    1

--- src/main/java/Evan.java
+++ src/main/java/Evan.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
+
 public class Evan {
     public static void main(String[] args) {
-        String greeting = "____________________________________________________________\n"
-                + " Hello! I'm Evan\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(greeting);
+        String lineSeparator = "____________________________________________________________";
+        System.out.println(lineSeparator);
+        System.out.println(" Hey! I'm Evan");
+        System.out.println(" What's up?");
+        System.out.println(lineSeparator);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {
+                System.out.println(lineSeparator);
+                System.out.println("Don't leave me ):");
+                System.out.println(lineSeparator);
+                break;
+            }
+            System.out.println(lineSeparator);
+            System.out.println(" " + input);
+            System.out.println(lineSeparator);
+        }
+        scanner.close();
     }
 }
