503f15bf333633193d04bf2ed1f6016427f9d1f1
yonktheskill <50507894+yonktheskill@users.noreply.github.com>
2026-02-20 04:11:03 +0800

    Update build.gradle

---
+++ src/main/java/evan/Launcher.java
@@ -0,0 +1,10 @@
+package evan;
+
+/**
+ * A launcher class to workaround classpath issues with JavaFX 11+.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
